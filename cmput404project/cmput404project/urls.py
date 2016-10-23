7e211276b45d10f9c056774892f8410508918a99
Wyatt <wrflemin@gmail.com>
2016-10-23 14:07:33 -0600

    Implementing users and groups in the backend (the app has the name service)

--- cmput404project/cmput404project/urls.py
+++ cmput404project/cmput404project/urls.py
@@ -16,12 +16,17 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
+from rest_framework import routers
+from service import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
 
 
 urlpatterns = [
     url(r'^', include('posts.urls')),
+    url(r'^', include(router.urls)),
     url(r'^admin/', admin.site.urls),
-    url(r'^login/$', auth_views.login, name='login'),
-    url(r'^logout/$', auth_views.logout, name='logout'),
-
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
--- cmput404project/posts/views.py
+++ cmput404project/posts/views.py
@@ -2,6 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-@login_required(login_url='/login/')
 def index(index):
     return HttpResponse("Hello, world. You're at the polls index.")
