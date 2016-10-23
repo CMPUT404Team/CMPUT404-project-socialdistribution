4258d42ba2fadfbffe32986033b29ab1b2078639
Wyatt <wrflemin@gmail.com>
2016-10-23 15:28:23 -0600

    Adding test for getting a user from the api

--- cmput404project/service/tests.py
+++ cmput404project/service/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from django.contrib.auth.models import User
+from rest_framework.test import APITestCase, APIClient, force_authenticate
+from django.urls import reverse
+from rest_framework import status
 
-# Create your tests here.
+from .views import UserViewSet
+
+class UserViewSetTests(APITestCase):
+    def setUp(self):
+        User.objects.create_superuser('superuser', 'test@test.com', 'test1234')
+    def test_get_user(self):
+        """
+        Ensure we can create a new account object.
+        """
+        factory = APIRequestFactory()
+        user = User.objects.get(username='superuser')
+        view = UserViewSet.as_view({'get':'list'})
+
+        # Make an authenticated request to the view...
+        request = factory.get('/users/')
+        force_authenticate(request, user=user)
+        response = view(request)	 	
+        self.assertEqual(response.status_code, 200)
+        response.render()
+        self.assertIn('"username":"superuser"', response.content)
+
+    def test_create_user(self):
+        pass
