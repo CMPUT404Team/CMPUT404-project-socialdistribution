408ca91ee811ebc65b1518aa108eaeff96c36678
LeahGabrielle <olexson@ualberta.ca>
2016-10-26 18:07:45 -0600

    started comment tests.py and Comment.py

--- cmput404project/service/models/Comment.py
+++ cmput404project/service/models/Comment.py
@@ -1,11 +1,16 @@
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
+import uuid
 
 @python_2_unicode_compatible
 class Comment(models.Model):
     author = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    comment=""
-    def __init__(self,comm):
-        comment=comm
-    
+    pubDate = models.DateTimeField('date published')
+    comment= models.CharField(max_length=200)
+    guid = models.CharField(primary_key=True, max_length=200)
+
+    @classmethod
+    def create_comment(cls,comm, auth):
+        createdComment = cls(comment = comm, author = auth)
+        createdComment.guid = uuid.uuid4()
+        return createdComment
--- cmput404project/service/tests.py
+++ cmput404project/service/tests.py
@@ -8,7 +8,7 @@ from .views import UserViewSet
 
 class UserViewSetTests(APITestCase):
     def setUp(self):
-        superuser = User.objects.create_superuser('superuser', 'test@test.com', 'test1234')       
+        superuser = User.objects.create_superuser('superuser', 'test@test.com', 'test1234')
         self.client = APIClient()
         #Authenticate as a super user so we can test everything
         self.client.force_authenticate(user=superuser)
@@ -18,7 +18,7 @@ class UserViewSetTests(APITestCase):
         self.assertEqual(response.status_code, 200)
         response.render()
         self.assertIn('"username":"superuser"', response.content)
-    
+
     def test_get_invalid_user(self):
         response = self.get_user(999)
         self.assertEqual(response.status_code, 404)
@@ -36,12 +36,37 @@ class UserViewSetTests(APITestCase):
         User.objects.create_user(username='ExistingUser')
         response = self.create_user('ExistingUser')
         self.assertEqual(response.status_code, 400)
-    
+
     def get_user(self, user_id):
         """
         Ensure we can create a new account object.
         """
         return self.client.get('/users/'+str(user_id)+'/')
-    
+
     def create_user(self, username):
         return self.client.post('/users/', {"username":username}, format='json')
+
+
+class CommentTests(APITestCase):
+
+    def setUp(self):
+        #make author, post
+        author = User.objects.create_superuser('superuser', 'test@test.com', 'test1234')
+        post = (post)
+        self.client = APIClient()
+        #Authenticate as a super user so we can test everything
+        self.client.force_authenticate(user=superuser)
+
+    def test_get_comment(self):
+        response = self.get_user(1)
+        comment = Comment.create_comment("hello I am a comment", author)
+        self.assertEqual(response.status_code, 200)
+        response.render()
+        self.assertIn('JSON GOES HERE', response.content)
+
+    def test_post_comment(self):
+        response = self.get_user(1)
+        comment = Comment.create_comment("hello", author)
+        self.assertEqual(response.status_code, 200)
+        response.render()
+        self.assertIn('JSON GOES HERE', response.content)
