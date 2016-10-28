b01680fd0ee55f6e6ab1c8174dc4285ac9ea1100
sensible-heart <alexandra.makepeace@gmail.com>
2016-10-28 16:38:06 -0600

    Added pass to each test method to prevent syntax errors.

--- cmput404project/service/tests.py
+++ cmput404project/service/tests.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework.test import APITestCase, APIClient, force_authenticate
 from django.urls import reverse
 from rest_framework import status
-from models import Author
+from service.models import Author
 
 from .views import UserViewSet
 
@@ -60,16 +60,17 @@ class PostViewSetTests(APITestCase):
         author = Author.objects.create_author('Username')
         self.client.force_authenticate(user=author)
 
-    #Defining our GET endpoints that we need to test
     def get_posts_by_current_user(self):
-        #http://service/author/posts (posts that are visible to the currently authenticated user)
         return self.client.get('/author/posts/')
+        #http://service/author/posts (posts that are visible to the currently authenticated user)
 
     def test_get_posts_by_current_author(self):
         #TODO: Retrives all the posts made by the currently logged in author
+        pass
 
     def test_get_posts_by_current_admin(self):
         #TODO: Retrieves all the posts made by the currently logged in admin
+        pass
 
     def get_public_posts(self):
         # http://service/posts (all posts marked as public on the server)
@@ -77,6 +78,7 @@ class PostViewSetTests(APITestCase):
 
     def test_get_public_posts(self):
         #TODO: Retrieve all public posts on the server
+        pass
 
     def get_posts_by_author_id(self, author_id):
         # http://service/author/{AUTHOR_ID}/posts (all posts made by {AUTHOR_ID} visible to the currently authenticated user)
@@ -84,9 +86,11 @@ class PostViewSetTests(APITestCase):
 
     def test_get_posts_by_author_id(self):
         #TODO: returns all the posts made by an author with a specific ID
+        pass
 
     def test_get_posts_with_invalid_author_id(self):
         #TODO: test getting posts with an invalid author id, or author ID that doesn't exist
+        pass
 
     def get_single_post_by_id(self, post_id):
         # http://service/posts/{POST_ID} access to a single post with id = {POST_ID}
@@ -94,10 +98,12 @@ class PostViewSetTests(APITestCase):
 
     def test_get_post_by_id(self):
         #TODO: Retrieves the post by its unique ID
+        pass
 
     def test_get_posts_with_invalid_post_id(self):
         #TODO: tests behaviour for when you get posts with incorrectly
         #      formatted ID, or ID that doesn't exist
+        pass
 
     def get_posts_by_page(self, page_number):
         # GET http://service/author/posts?page=4
@@ -105,19 +111,24 @@ class PostViewSetTests(APITestCase):
 
     def test_get_posts_by_page(self):
         #TODO: Returns all of posts on a specific page
+        pass
 
     def test_get_full_page_of_posts(self):
         #TODO: test retrieving a full page of posts
+        pass
 
     def test_get_partial_page_of_posts(self):
         #TODO: test retrieving a partial page of posts
+        pass
 
     def test_page_does_not_exist(self):
         #TODO: Tests what is returned if requested page of posts does not exist
+        pass
 
     def test_page_has_no_posts(self):
         #TODO: Tests getting page 1, when the user has made no posts
         #pretty sure this is redundant to above
+        pass
 
     def get_posts_by_page_and_size(self, page_number, size):
         # GET http://service/author/posts?page=4&size=50
@@ -125,12 +136,15 @@ class PostViewSetTests(APITestCase):
 
     def test_get_posts_by_page_and_size(self):
         #TODO: retrieve a page of posts with specific size of page
+        pass
 
     def test_get_posts_by_page_and_exceeded_size(self):
         #TODO: retrieve a page where there are more posts than the specified size
+        pass
 
     def test_get_posts_by_page_and_partial_size(self):
         #TODO: retrive a page where the size is smaller than the one specified
+        pass
 
     def create_update_post(self, post_id):
         #PUT http://service/posts/postid to update/create post
@@ -138,32 +152,41 @@ class PostViewSetTests(APITestCase):
 
     def test_create_post_with_put(self):
         #TODO: Create a post using a put method
+        pass
 
     def test_update_post(self):
         #TODO: Update an existing post
+        pass
 
     def test_update_nonexistent_post(self):
         #TODO: try to update a post that hasn't been created
+        pass
 
     def test_create_invalid_post(self):
         #TODO: create a post that has an invalid post_id
         # maybe unessecary?
+        pass
 
     def test_create_existing_post(self):
         #TODO: create a post that already exists with a put
+        pass
 
     def create_post(self, post_id):
         #a POST should insert the post http://service/posts/postid
-        return self.client.post('/posts/'+str(post_id)+'/'{"postid":post_id}, format='json')
+        return self.client.post('/posts/'+str(post_id)+'/',{"postid":post_id}, format='json')
 
     def test_create_post_with_post(self):
         #TODO: Create a post using a post method
+        pass
 
     def test_update_post_with_post(self):
         #TODO: Update an existing post
+        pass
 
     def test_insert_into_existing(self):
         #TODO: try to post into an existing post
+        pass
 
     def test_insert_invalid_post(self):
         #TODO: insert an invalidly formatted post
+        pass
