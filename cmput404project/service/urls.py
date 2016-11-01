cec1e06a3bc16499fe85920cec4b0747b11ba756
Oleksii Shevchenko <shevaroller@gmail.com>
2016-11-01 15:04:53 -0600

    add API functionality to get all posts, create new post

--- cmput404project/service/urls.py
+++ cmput404project/service/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
+    url(r'^posts/$', views.PostsView.as_view(), name='posts'),
     url(r'^posts/(?P<uuid>[^/]+)/$', views.PostView.as_view(), name='post')
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- cmput404project/service/views.py
+++ cmput404project/service/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.models import User, Group
-from rest_framework import generics, viewsets
+from rest_framework import generics, viewsets, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from service.serializers import UserSerializer, GroupSerializer, PostSerializer
@@ -21,18 +21,52 @@ class GroupViewSet(viewsets.ModelViewSet):
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
 
-class PostView(APIView):
+class PostsView(APIView):
+    """
+    Return a list of all posts or create a new post
+    """
+    def get(self, request, format=None):
+        posts = Post.objects.all()
+        serializer = PostSerializer(posts, many=True)
+        return Response(serializer.data)
 
+    def post(self, request, format=None):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class PostView(APIView):
+    """
+    Get, update or delete a particular post
+    """
     def get_object(self, uuid):
-            try:
-                    return Post.objects.get(id=uuid)
-            except Post.DoesNotExist:
-                    raise Http404
+        try:
+            return Post.objects.get(id=uuid)
+        except Post.DoesNotExist:
+            raise Http404
 
     def get(self, request, uuid):
-            post = self.get_object(uuid)
-            serializer = PostSerializer(post)
+        post = self.get_object(uuid)
+        serializer = PostSerializer(post)
+        return Response(serializer.data)
+
+    def post(self, request, uuid):
+        post = self.get_object(uuid)
+        serializer = PostSerializer(post)
+        if serializer.is_valid():
+            serializer.save()
             return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, uuid):
+        post = self.get_object(uuid)
+        serializer = PostSerializer(post)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class PostViewSet(viewsets.ModelViewSet):
     """
