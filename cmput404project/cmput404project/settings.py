c3947d9c52e6387aeac6d7bfaa2e5e6da5c6d95f
Wyatt <wrflemin@gmail.com>
2016-11-19 12:49:26 -0700

    migrating to PostGres

--- cmput404project/cmput404project/settings.py
+++ cmput404project/cmput404project/settings.py
@@ -84,14 +84,20 @@ WSGI_APPLICATION = 'cmput404project.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'service',
+        'USER': 'serviceuser',
+        'PASSWORD': os.environ['SERVICE_PASSWORD'],
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
