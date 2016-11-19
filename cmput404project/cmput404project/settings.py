afef12cc15df0f847d31b227e6da897faf4af174
Wyatt <wrflemin@gmail.com>
2016-11-19 16:46:33 -0700

    taking out an unnecesary env var

--- cmput404project/cmput404project/settings.py
+++ cmput404project/cmput404project/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'service',
-        'USER': os.environ['SERVICE_USER'],
+        'USER': serviceuser,
         'PASSWORD': os.environ['SERVICE_PASSWORD'],
         'HOST': 'localhost',
         'PORT': '5432',
