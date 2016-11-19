b81ec8d90ce2aebd042141c2f078c50dcb468d8c
Megan Sumner <msumner@ualberta.ca>
2016-11-19 14:37:19 -0700

    Added friend API test

--- cmput404project/service/urls.py
+++ cmput404project/service/urls.py
@@ -10,8 +10,8 @@ urlpatterns = [
     url(r'^posts/(?P<pid>[^/]+)/comments', views.CommentAPIView.as_view()),
     url(r'^author/add/$', views.AuthorCreate.as_view(), name='author-add'),
     url(r'^friendrequest/$', views.FriendRequestView.as_view(), name='friend-request'),
-    url(r'^friends/(?P<uuid1>[^/]+)/(?P<uuid2>[^/]+)$', views.FriendDetailView.as_view()),
-    url(r'^friends/(?P<uuid>[^/]+)/', views.MutualFriendDetailView.as_view()),
+    url(r'^friends/(?P<uuid1>[^/]+)/(?P<uuid2>[^/]+)$', views.FriendDetailView.as_view(), name='friend-detail'),
+    url(r'^friends/(?P<uuid>[^/]+)/', views.MutualFriendDetailView.as_view(),name='mutual-friend'),
     url(r'^author/(?P<pk>[^/]+)/$', views.AuthorDetailView.as_view(), name='author-detail')
 ]
 
