677c57a81a211d7e9133444bd3a7b598f5e56704
Oleksii Shevchenko <shevaroller@gmail.com>
2016-11-19 18:15:36 -0700

    add row tests for node model

--- cmput404project/service/testNodeModel.py
+++ cmput404project/service/testNodeModel.py
@@ -10,19 +10,34 @@ class NodeModelTests(TestCase):
     def setUp(self):
         su_username = "superuser"
         su_password = "test1234"
-        remote_username = "testsuperuser"
-        remote_password = "testpassword"
+        self.remote_username = "testsuperuser"
+        self.remote_password = "testpassword"
         superuser = User.objects.create_superuser(su_username, 'test@test.com', su_password)
         self.node = Node.create(
-            displayName = "Home Node",
+            displayName = "The Node",
             baseUrl = "http://localhost:8000/",
             user = superuser,
-            username = remote_username,
-            password = remote_password 
+            username = self.remote_username,
+            password = self.remote_password 
             )
         self.node.save()
+
+    def test_node_creates_id(self):
+        self.assertIsNotNone(self.node.id)
+
+    def test_node_displayName_equal(self):
+        self.assertEqual(self.node.displayName, "The Node")
+
+    def test_node_baseUrl_equal(self):
+        self.assertEqual(self.node.baseUrl, "http://localhost:8000/")
+
+    def test_node_username_equal(self):
+        self.assertEqual(self.node.username, self.remote_username)
+
+    def test_node_password_equal(self):
+        self.assertEqual(self.node.password, self.remote_password)
 
     def test_get_posts(self):
         posts = self.node.get_posts()
         print posts
-        self.assertEqual([], posts)
+        self.assertTrue(hasattr(posts, '__iter__'))
