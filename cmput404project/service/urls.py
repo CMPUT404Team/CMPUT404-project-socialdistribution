feb1128e0994481988e24d156e728dfca4749946
Megan Sumner <msumner@ualberta.ca>
2016-11-20 17:29:16 -0700

    Got postgres database working

--- cmput404project/service/urls.py
+++ cmput404project/service/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     url(r'^friendrequest/$', views.FriendRequestView.as_view(), name='friend-request'),
     url(r'^friends/(?P<uuid1>[^/]+)/(?P<uuid2>[^/]+)$', views.FriendDetailView.as_view(), name='friend-detail'),
     url(r'^friends/(?P<uuid>[^/]+)/', views.MutualFriendDetailView.as_view(),name='mutual-friend'),
-    url(r'^author/(?P<pk>[^/]+)/$', views.AuthorDetailView.as_view(), name='author-detail')
+    url(r'^author/(?P<pk>[^/]+)/$', views.AuthorDetailView.as_view(), name='author-detail'),
     url(r'^author/awaiting-approval$', TemplateView.as_view(template_name='awaiting-approval.html'), name='awaiting-approval')
 ]
 
