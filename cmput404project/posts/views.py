1d3ec4e54000704842304fb910e0f7beda04c8bd
sensible-heart <alexandra.makepeace@gmail.com>
2016-11-22 17:40:41 -0700

    Added an independent front end for /posts/ in posts app.

--- cmput404project/posts/views.py
+++ cmput404project/posts/views.py
@@ -2,6 +2,30 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
+from rest_framework.renderers import TemplateHTMLRenderer
+import urllib2, base64
+import json
+from rest_framework.views import APIView
+from django.template.response import TemplateResponse
+import os
 
 def index(index):
     return redirect("author-add")
+
+class PostsView(APIView):
+    '''
+    '''
+    username = 'dogordie'
+    #username = 'alex'
+    #password='test1234'
+    password = os.environ.get('FRONTEND_PASSWORD')
+    print password
+    host = 'http://127.0.0.1:8000/'
+    def get(self, request):
+        url = self.host + 'posts/'
+        req = urllib2.Request(url)
+        base64string = base64.b64encode('%s:%s' % (self.username, self.password))
+        req.add_header("Authorization", "Basic %s" % base64string)
+        serialized_data = urllib2.urlopen(req).read()
+        posts = json.loads(serialized_data)
+        return render(request, "posts.html", {"posts":posts})
--- cmput404project/service/views.py
+++ cmput404project/service/views.py
@@ -16,7 +16,6 @@ from django.urls import reverse
 from django.forms import modelformset_factory
 from django.shortcuts import render
 from django.views.generic.edit import FormView
-from rest_framework.renderers import TemplateHTMLRenderer
 from AuthorForm import AuthorForm
 from django.core.exceptions import SuspiciousOperation
 import json
@@ -103,12 +102,10 @@ class PostsView(APIView):
     """
     Return a list of all public posts or create a new post
     """
-    template_name ="posts.html"
-    renderer_classes = [TemplateHTMLRenderer]
     def get(self, request):
         posts = Post.objects.all().filter(visibility="PUBLIC")
         serializer = PostSerializerGet(posts, many=True, context={'request':request})
-        return Response({'serializer': serializer.data, 'posts': posts})
+        return Response(serializer.data)
 
     def post(self, request):
         serializer = PostSerializerPutPost(data=request.data, context={'request':request})
@@ -121,8 +118,6 @@ class PostView(APIView):
     """
     Get, update or delete a particular post
     """
-    template_name ="posts-id.html"
-    renderer_classes = [TemplateHTMLRenderer]
     def get_object(self, uuid):
         try:
             return Post.objects.get(id=uuid)
@@ -136,7 +131,7 @@ class PostView(APIView):
         comments = Comment.objects.filter(post_id=pk)
         post.comments = comments
         serializer = PostSerializerGet(post, context={'request':request})
-        return Response({'serializer': serializer.data, 'post': post})
+        return Response(serializer.data)
 
     def post(self, request, pk):
         try:
