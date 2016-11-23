a3e3528341b5caef0511ca125fdb3a339c3fca0b
Megan Sumner <msumner@ualberta.ca>
2016-11-23 01:23:02 -0700

    changed url from frontend to dogo

--- cmput404project/posts/urls.py
+++ cmput404project/posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
             url(r'^$', views.index, name='index'),
-            url(r'^frontend/posts/$', views.PostsView.as_view(), name='publicPosts'),
-            url(r'^frontend/friends/$', views.FriendView.as_view(), name='friend-detail'),
-            url(r'^frontend/author/posts/$', views.AuthorPostsView.as_view(), name='authorPosts')
+            url(r'^dogo/posts/$', views.PostsView.as_view(), name='publicPosts'),
+            url(r'^dogo/friends/$', views.FriendView.as_view(), name='friend-detail'),
+            url(r'^dogo/author/posts/$', views.AuthorPostsView.as_view(), name='authorPosts')
             ]
