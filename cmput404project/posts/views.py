be67170336d067f45d99c7c204aae80dfec5d499
Megan Sumner <msumner@ualberta.ca>
2016-11-22 22:38:29 -0700

    changed url

--- cmput404project/posts/views.py
+++ cmput404project/posts/views.py
@@ -22,7 +22,7 @@ class PostsView(APIView):
     # host = 'http://localhost:8000/'
     host = 'http://winter-resonance.herokuapp.com/'
     def get(self, request):
-        url = self.host + 'posts/'
+        url = self.host + 'frontend/posts/'
         req = urllib2.Request(url)
         base64string = base64.b64encode('%s:%s' % (self.username, self.password))
         req.add_header("Authorization", "Basic %s" % base64string)
@@ -30,16 +30,19 @@ class PostsView(APIView):
         posts = json.loads(serialized_data)
         return render(request, "posts.html", {"posts":posts})
 
+
 class FriendView(APIView):
     username = 'dogordie'
     password = os.environ.get('FRONTEND_PASSWORD')
-    # username = 'admin'
-    # password = 'superuser'
+    username = 'admin'
+    password = 'superuser'
     host = 'http://winter-resonance.herokuapp.com/'
-    # host = 'http://localhost:8000/'
+    host = 'http://localhost:8000/'
     def get(self, request):
-	print (str(request.user.id))
-        url = self.host + 'friends' + '/cccd2900-b54b-4a56-b0fe-421f04bfeafa'
+        user = request.user.username
+        
+        print str(user.username)
+        url = self.host + 'friends/' + str(user.username)
         req = urllib2.Request(url)
         base64string = base64.b64encode('%s:%s' % (self.username, self.password))
         req.add_header("Authorization", "Basic %s" % base64string)
