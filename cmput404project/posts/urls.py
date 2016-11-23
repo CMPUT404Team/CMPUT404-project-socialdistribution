4f89a01dea2305a707e456c582598580ecc804f9
Wyatt <wrflemin@gmail.com>
2016-11-23 00:12:29 -0700

    reverting

--- cmput404project/posts/urls.py
+++ cmput404project/posts/urls.py
@@ -5,7 +5,4 @@ from . import views
 
 urlpatterns = [
             url(r'^$', views.index, name='index'),
-            url(r'^frontend/posts/$', views.PostsView.as_view(), name='publicPosts'),
-            url(r'^frontend/friends/$', views.FriendView.as_view(), name='friend-detail'),
-            url(r'^frontend/author/posts/$', views.AuthorPostsView.as_view(), name='authorPosts')
             ]
