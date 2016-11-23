01cce133b254ba56b7bc482da4a2f55b5b99a679
Megan Sumner <msumner@ualberta.ca>
2016-11-22 20:42:53 -0700

    changed urls

--- cmput404project/posts/urls.py
+++ cmput404project/posts/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
             url(r'^$', views.index, name='index'),
-            url(r'^frontend/posts/$', views.PostsView.as_view(), name='publicPosts')
+            url(r'^frontend/posts/$', views.PostsView.as_view(), name='publicPosts'),
+            url(r'^frontend/friends/$', views.FriendView.as_view(), name='friend-detail')
             ]
--- cmput404project/posts/views.py
+++ cmput404project/posts/views.py
@@ -16,10 +16,11 @@ class PostsView(APIView):
     '''
     '''
     username = 'dogordie'
-    #username = 'alex'
-    #password='test1234'
+    # username = 'admin'
+    # password='superuser'
     password = os.environ.get('FRONTEND_PASSWORD')
-    host = 'http://localhost:5000/'
+    # host = 'http://localhost:8000/'
+    host = 'http://winter-resonance.herokuapp.com'
     def get(self, request):
         url = self.host + 'posts/'
         req = urllib2.Request(url)
@@ -28,3 +29,20 @@ class PostsView(APIView):
         serialized_data = urllib2.urlopen(req).read()
         posts = json.loads(serialized_data)
         return render(request, "posts.html", {"posts":posts})
+
+class FriendView(APIView):
+    username = 'dogordie'
+    password = os.environ.get('FRONTEND_PASSWORD')
+    # username = 'admin'
+    # password = 'superuser'
+    host = 'http://winter-resonance.herokuapp.com'
+    # host = 'http://localhost:8000/'
+    def get(self, request):
+	print (str(request.user.id))
+        url = self.host + 'friends' + '/cccd2900-b54b-4a56-b0fe-421f04bfeafa'
+        req = urllib2.Request(url)
+        base64string = base64.b64encode('%s:%s' % (self.username, self.password))
+        req.add_header("Authorization", "Basic %s" % base64string)
+        serialized_data = urllib2.urlopen(req).read()
+        friends = json.loads(serialized_data)
+        return render(request, "friends.html", {"friends":friends})
